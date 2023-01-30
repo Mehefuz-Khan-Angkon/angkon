af400168af47796126931326e0b857cb391fce73
Mehefuz Khan Angkon <mehefuz.angkan@gmail.com>
2023-01-30 14:26:06 +0600

    initial commit

---
+++ listings/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .models import Listing
+# Create your views here.
+
+
+
+
+def listing_list(request):
+	listings = Listing.objects.all()
+	context = {
+
+	    "listings": listings
+	}
+	return render(request, "listings.html",context)
+
+
+
+def listing_retrieve(request, pk):
+	listing = Listing.objects.get(id=pk)
+	context = {
+	     "listings": listing
+	}
+
+	return render(request, "listing.html",context)
+
+
+
